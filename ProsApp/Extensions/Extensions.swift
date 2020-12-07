58f5da56456a9b1d7c62a3974df77e8a83759d95
gjh <garyjh99@gmail.com>
2020-12-07 12:18:03 -0500

    removed unused code...

--- ProsApp/Extensions/Extensions.swift
+++ ProsApp/Extensions/Extensions.swift
@@ -17,31 +17,3 @@ extension UIColor {
   }
     
 }
-
-extension UILabel {
-    
-   public func displayRating(rating: String, ratingCount: String) -> Void {
-        
-        let ratingFloat = Float(rating) ?? 0
-        let ratingCountInt = Int(ratingCount) ?? 0
-        var ratingsColor = UIColor.black
-
-        let displayText = ratingCountInt > 0 ?
-            "Ratings: \(rating) | \(ratingCount)" :
-            "References Available"
-    
-        if ratingCountInt > 0 {
-            if ratingFloat >= 4.0 {
-                ratingsColor = UIColor.RatingsColor.excellent
-            } else if ratingFloat >= 3.0 && ratingFloat < 4.0 {
-                ratingsColor = UIColor.RatingsColor.good
-            } else if ratingFloat < 3.0 {
-                ratingsColor = UIColor.RatingsColor.fair
-            }
-        }
-        
-        self.textColor = ratingsColor
-        self.text = displayText
-    }
-    
-}
